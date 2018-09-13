0a8789538268bfaf80fa7f989138af74d3c7ec99
michaelvickdev <michael.vick.hello@gmail.com>
2018-09-12 22:05:51 -0400

    Handle Error when api turn failed

--- resources/assets/js/Utils/ApiUtil.js
+++ resources/assets/js/Utils/ApiUtil.js
@@ -2,55 +2,68 @@ const serialize = (obj, prefix) => {
     const str = [];
     Object.keys(obj).forEach((p) => {
         const k = prefix ? prefix + "[" + p + "]" : p;
-    const v = obj[p];
-    str.push((v !== null && typeof v === "object") ?
-        serialize(v, k) : k + "=" + v);
-});
+        const v = obj[p];
+        str.push((v !== null && typeof v === "object") ?
+            serialize(v, k) : k + "=" + v);
+    });
     return str.join("&");
 };
 
 const fetchFromApi = (url, params) => {
     return new Promise((resolve) => {
-    fetch(`${url}?${serialize(params)}`, {
-        credentials: 'same-origin'
-    })
-        .then((result) => result.json())
-.then((response) => resolve(response));
-});
+        fetch(`${url}?${serialize(params)}`, {
+            credentials: 'same-origin'
+        })
+            .then((result) => result.json())
+            .then((response) => resolve(response));
+    });
 };
 
 const putToApi = (url, params) => {
     return new Promise((resolve) => {
-    fetch(`${url}?${serialize(params)}`, {
-        method: 'put',
-        credentials: 'same-origin'
-    })
-        .then((result) => result.json())
-.then((response) => resolve(response));
-});
+        fetch(`${url}?${serialize(params)}`, {
+            method: 'put',
+            credentials: 'same-origin'
+        })
+            .then((result) => result.json())
+            .then((response) => resolve(response));
+    });
 
 };
 const postToApi = (url, data) => {
-    return new Promise((resolve) => {
-    fetch(`${url}`, {
-        method: 'post',
-        body: data,
-        credentials: 'same-origin'
-    })
-        .then((result) => result.json())
-.then((response) => resolve(response));
-});
+    return new Promise((resolve, reject) => {
+        fetch(`${url}`, {
+            method: 'post',
+            body: data,
+            credentials: 'same-origin'
+        })
+            .then(function (response) {
+                if (!response.ok) {
+                    reject(response.headers.get('statustext'));
+                    throw Error(response.headers.get('statustext'));
+                }
+                resolve(response)
+                return response;
+            }).then(function (response) {
+            resolve(response)
+        }).catch(function (error) {
+            console.log(error);
+
+            reject(error);
+        })
+
+    });
 };
 
 const deleteToApi = (url, params) => {
     return new Promise((resolve) => {
-    fetch(`${url}?${serialize(params)}`, {
-        method: 'delete',
-        credentials: 'same-origin'
-    })
-        .then((result) => result.json())
-.then((response) => resolve(response));
-});
+        fetch(`${url}?${serialize(params)}`, {
+            method: 'delete',
+            credentials: 'same-origin'
+        })
+            .then((result) => result.json())
+            .then((response) => resolve(response));
+    });
 };
 
 module.exports = {
